3e41aff173c1539f5e7855b83e58dfb868587e9c
Daniel <josedanielalves018@gmail.com>
2024-10-23 15:30:19 -0300

    comentario sobre porta udp

--- TCPServer.h
+++ TCPServer.h
@@ -78,7 +78,7 @@ public:
      * 
      * @param file_name Nome do arquivo cujos chunks estão sendo solicitados.
      * @param requested_chunks Vetor contendo os IDs dos chunks que devem ser transferidos.
-     * @param destination_info Informações sobre o peer que está solicitando os chunks, incluindo seu endereço IP e porta.
+     * @param destination_info Informações sobre o peer que está solicitando os chunks, incluindo seu endereço IP e porta UDP (Porta TCP = Porta UDP + 1000).
      */
     void sendChunk(const std::string& file_name, const std::vector<int>& requested_chunks, const PeerInfo& destination_info);
 };
